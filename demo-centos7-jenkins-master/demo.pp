24b9127694285636799f8d5c1ef3d6fffdf574da
jamesread <contact@jwread.com>
2015-05-18 17:56:26 +0100

    Silly puppet is silly.

--- demo-centos7-jenkins-master/demo.pp
+++ demo-centos7-jenkins-master/demo.pp
@@ -1,5 +1,9 @@
-exec { "sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo" : }
-exec { "sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key" : }
+exec { "sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo" : 
+	path => "/" 
+}
+exec { "sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key" : 
+	path => "/" 
+}
 
 package { [ 
 	"httpd",
