b630ba41ead2175abcd26eeba757fa99615872fb
jamesread <contact@jwread.com>
2015-05-18 18:46:24 +0100

    Looks like we need fully qualified paths for executables

--- demo-centos7-jenkins-master/demo.pp
+++ demo-centos7-jenkins-master/demo.pp
@@ -1,7 +1,7 @@
-exec { "wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo" : 
+exec { "/usr/bin/wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo" : 
 	path => "/" 
 }
-exec { "rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key" : 
+exec { "/usr/bin/rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key" : 
 	path => "/" 
 }
 
