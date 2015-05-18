f5b0441cc8c26d1c017554a26da11e30c3f38ea3
jamesread <contact@jwread.com>
2015-05-18 18:16:35 +0100

    A few steps closer to a working Jenkins instance

--- demo-centos7-jenkins-master/demo.pp
+++ demo-centos7-jenkins-master/demo.pp
@@ -1,14 +1,25 @@
-exec { "sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo" : 
+exec { "wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo" : 
 	path => "/" 
 }
-exec { "sudo rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key" : 
+exec { "rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key" : 
 	path => "/" 
 }
 
 package { [ 
 	"httpd",
-	"mariadb-server" 
+	"mariadb-server",
+	"jenkins"
 	] :
 	ensure => installed,
 	allow_virtual => true
+}
+
+service { "firewalld": 
+	ensure => stopped,
+	enabled => false
+}
+
+service { "jenkins": 
+	ensure => started,
+	enabled => true
 }
